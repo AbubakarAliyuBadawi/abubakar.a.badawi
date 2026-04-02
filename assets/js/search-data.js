dfbe23866c0882e9f23e6cc360d6a93a37459d20
AbubakarAliyuBadawi <abubakb@stud.ntnu.no>
2026-04-02 14:34:51 +0000

    Deploying to gh-pages from @ AbubakarAliyuBadawi/abubakar.a.badawi@d8c029abe6bbffee5b6ff494dfca6eafd38edbd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "PhD Researcher in Autonomous Systems and Risk Modeling at NTNU. Download my CV or view it below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/abubakar.a.badawi/cv/";
